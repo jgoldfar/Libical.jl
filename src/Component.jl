9dea4a9b8355c6e40972dd2e9ce286b8e8e5401d
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-05-14 13:50:38 -0400

    Cleanup Component subcomponents interface

--- src/Component.jl
+++ src/Component.jl
@@ -51,23 +51,30 @@ kindof(c::Component) = icalcomponent_isa(c.ref)
 
 kindString(c::Component) = unsafe_string(icalcomponent_kind_to_string(kindof(c)))
 
-function components(c::Component, kind::String = "ANY")
-    kindVal = icalcomponent_string_to_kind(kind)
-    numComponents = icalcomponent_count_components(c.ref, kindVal)
-    compVec = Component{Ptr{icalcomponent}}[]
+function components(c::Component, kind::UInt32 = ICAL_ANY_COMPONENT)
+    numComponents = icalcomponent_count_components(c.ref, kind)
+    compVec = typeof(c)[]
     if numComponents == 0
         return compVec
     end
 
-    cint = icalcomponent_get_first_component(c.ref, kindVal)
+    cint = icalcomponent_get_first_component(c.ref, kind)
 
     while cint != C_NULL
         push!(compVec, Component(cint))
 
-        cint = icalcomponent_get_next_component(c.ref, kindVal)
+        cint = icalcomponent_get_next_component(c.ref, kind)
     end
     compVec
 end
+function components(c::Component, kind::String)
+    if isempty(kind)
+        components(c)
+    else
+        components(c, icalcomponent_string_to_kind(kind))
+    end
+end
+
 function inner_component(c::Component)
     innerComp = icalcomponent_get_inner(c.ref)
     if innerComp == C_NULL
@@ -84,4 +91,60 @@ function add_component(c::Component, c1::Component)
 end
 function remove_component(c::Component, c1::Component)
     icalcomponent_remove_component(c.ref, c1.ref)
+end
+
+_prop_from_ref(c::Component, p::Property) = _prop_from_ref(c::Component, p.ref)
+
+function _prop_from_ref(c::Component, p)
+    if p == C_NULL
+        return nothing
+    end
+    propValue = icalproperty_get_value_as_string(p)
+    propName = icalproperty_get_property_name(p)
+
+    propKind = icalproperty_string_to_kind(propName)
+    propKindValue = icalproperty_kind_to_value_kind(propKind)
+
+    propValueType = icalvalue_kind_to_string(propKindValue)
+    propRef = p
+    if (propKindValue == "DATE-TIME" )|| propKindValue == "DATE"
+        prop = Time(p)
+    elseif propKindValue == "PERIOD"
+        prop = Period(p)
+    elseif propKindValue == "DURATION"
+        prop = Duration(p)
+    elseif propName == "ATTACH"
+        prop = Attach(p)
+    elseif propName == "ATTENDEE"
+        prop = Addendee(p)
+    elseif propName == "ORGANIZER"
+        prop = Organizer(p)
+    else
+        prop = Property(p)
+    end
+    prop
+end
+
+function properties(c::Component, kind::UInt32 = ICAL_ANY_PROPERTY)
+    numProps = icalcomponent_count_properties(c.ref, kind)
+    propVec = Property{Ptr{icalproperty}}[]
+    if numProps == 0
+        return propVec
+    end
+
+    pint = icalcomponent_get_first_property(c.ref, kind)
+
+    while pint != C_NULL
+        push!(propVec, Property(pint))
+
+        pint = icalcomponent_get_next_property(c.ref, kind)
+    end
+    propVec
+end
+function properties(c::Component, kind::String)
+    if isempty(kind)
+        properties(c)
+    else
+        properties(c, icalproperty_string_to_kind(kind))
+    end
 end
