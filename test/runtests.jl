ff137fea33f89ce47ef1a97c8d6e964748f2d2cb
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-08-01 14:19:48 -0400

    Test explicitly against v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using Libical
-@static if VERSION >= v"0.7-"
-    using Test
-else
+@static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
+else
+    using Test
 end
 
 @testset "Property" begin
